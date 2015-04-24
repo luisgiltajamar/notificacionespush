086c85d3a93dc6589ba9811273b9466f52188d51
Luis Felipe Gil <luisgil@net2mate.com>
2015-04-24 10:32:54 +0200

    A

---
+++ NotificacionesPush/services/mobileServices/scripts/NotificacionesPushDemo.push.register.js
@@ -0,0 +1,28 @@
+﻿// http://go.microsoft.com/fwlink/?LinkID=290993&clcid=0x409
+(function () {
+    "use strict";
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.addEventListener("activated", function (args) {
+        if (args.detail.kind == activation.ActivationKind.launch) {
+            // Note: in the simulator this code will go throw the error path
+            Windows.Networking.PushNotifications.
+                PushNotificationChannelManager.
+                createPushNotificationChannelForApplicationAsync()
+                .then(function (channel) {
+                    return NotificacionesPushDemoClient.push.
+                        registerNative(channel.uri);
+                })
+                .done(function (registration) {
+                    return NotificacionesPushDemoClient
+                        .invokeApi("notifyAllUsers",
+                        { body: { toast: "Sample Toast" } });
+                }, function (error) {
+                    // Error
+
+                });
+        }
+    });
+})();
---
+++ NotificacionesPush/services/mobileServices/settings/NotificacionesPushDemo.js
@@ -0,0 +1,4 @@
+﻿// http://go.microsoft.com/fwlink/?LinkID=290993&clcid=0x409
+var NotificacionesPushDemoClient = new WindowsAzure.MobileServiceClient(
+                "https://notificacionespushdemo.azure-mobile.net/",
+                "HxckfBoGJyPogfeowmeSwnkwrXKjUI16");
