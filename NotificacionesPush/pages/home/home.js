5534d808a18c89658f37b523c3b85aeb4ca8d7b0
Luis Felipe Gil <luisgil@net2mate.com>
2015-04-27 12:30:54 +0200

    A

--- NotificacionesPush/pages/home/home.js
+++ NotificacionesPush/pages/home/home.js
@@ -9,7 +9,7 @@
             NotificacionesPushDemoClient.invokeApi("usuarios",
         {method:"get"}).done(function(resu) {
                 var data = JSON.parse(resu.response);
-
+                sessionStorage.id = data.id;
                 guardarDatos(data);
             }, function(err) {
                 var a = err;
@@ -38,7 +38,8 @@
                                 type: "put",  responseType: "json",
                                 headers: {"content-Type":"application/json"},
                                 data: JSON.stringify(user)
-                            }).done(function() {
+                            }).done(function () {
+                                cambiarVista();
                                 return NotificacionesPushDemoClient.push.
                                     registerNative(channel.uri);
 
@@ -63,7 +64,8 @@
                                 data: dataTxt,
                                 responseType: "json",
                                 headers: { "content-Type": "application/json" }
-                            }).done(function() {
+                            }).done(function () {
+                                    cambiarVista();
                                 return NotificacionesPushDemoClient.push.
                                     registerNative(channel.uri);
 
@@ -78,35 +80,84 @@
                 });
     }
 
+    function cambiarVista() {
+        
+        var base_url =
+            "http://webapiusuariosfacebookpush.azurewebsites.net/api/";
+        WinJS.xhr({ url: base_url + "usuarios/", type: "GET" }).done(
+            function(res) {
+                var usr = JSON.parse(res.response);
+
+                for (var i = 0; i < usr.length; i++) {
+                    var ele = document.createElement("option");
+                    ele.setAttribute("value", usr[i].idFacebook);
+                    var txt = document.createTextNode(usr[i].nombre);
+                    ele.appendChild(txt);
+                    document.getElementById("ddlUsuario").appendChild(ele);
+                }
+
+                document.getElementById("login").style.display = "none";
+                document.getElementById("mensajes").style.display = "block";
+
+            }
+        );
+
+
+    }
+    function enviar() {
+        var base_url =
+            "http://webapiusuariosfacebookpush.azurewebsites.net/api/";
+        var ddl = document.getElementById("ddlUsuario");
+        var dest = ddl.options[ddl.selectedIndex].value;
+        var obj = {
+            idOrigen: sessionStorage.id,
+            idDestino: dest,
+            texto: document.getElementById("txtMensaje").value
+        }
+        var dataTxt = JSON.stringify(obj);
+        WinJS.xhr({
+            url: base_url + "mensajes",
+            type: "post",
+            data: dataTxt,
+            responseType: "json",
+            headers: { "content-Type": "application/json" }
+        }).done(function() {
+
+            WinJS.xhr({
+                url: base_url + "usuarios/" + dest,
+                type: "get",
+                data: dataTxt,
+                responseType: "json",
+                headers: { "content-Type": "application/json" }
+            }).done(function(res) {
+                var usuario = JSON.parse(res.response);
+                NotificacionesPushDemoClient.invokeApi("notificarmensaje",
+        { method: "post", body: { cadena: document.getElementById("txtMensaje").value, uri: "Facebook:"+dest } }).
+                    done(function () { }, 
+                    function(error) {
+                        var errorr = error;
+                    });
+            }, function(err) {
+                var e = err;
+
+            });
+
+
+        });
+
+
+    }
+
     WinJS.UI.Pages.define("/pages/home/home.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
-        ready: function (element, options) {
+        ready: function(element, options) {
             document.getElementById("btnLogin").addEventListener("click",
                 login);
-            document.getElementById("btnSubmit").addEventListener("click",
-                function () {
-                        var obj= {
-                            asunto: document.getElementById("txtAsunto").value,
-                            texto: document.getElementById("txtMensaje").value
+            document.getElementById("btnSubmit").addEventListener("click", enviar);
 
 
-                        }
-
-                        NotificacionesPushDemoClient.getTable("mensaje").
-                            insert(obj).done(function() {
-                           
-
-
-                            },
-                        function () {
-                           
-
-
-                        }
-                        );
-
-                });
         }
     });
+
 })();
