619b220ddd94f624adc572b30df4c73ffe734980
Luis Felipe Gil <luisgil@net2mate.com>
2015-04-27 10:10:17 +0200

    B

--- NotificacionesPush/pages/home/home.js
+++ NotificacionesPush/pages/home/home.js
@@ -1,11 +1,112 @@
 ﻿(function () {
     "use strict";
 
+
+    function login() {
+        NotificacionesPushDemoClient.login("facebook").done(function(res) {
+        
+
+            NotificacionesPushDemoClient.invokeApi("usuarios",
+        {method:"get"}).done(function(resu) {
+                var data = JSON.parse(resu.response);
+
+                guardarDatos(data);
+            }, function(err) {
+                var a = err;
+
+            });
+
+        });
+
+
+    }
+
+    function guardarDatos(facebook) {
+        var base_url =
+            "http://webapiusuariosfacebookpush.azurewebsites.net/api/";
+        WinJS.xhr({ url: base_url + "usuarios/" + facebook.id, type: "GET" })
+            .done(function(res) {
+                    Windows.Networking.PushNotifications.
+                        PushNotificationChannelManager.
+                        createPushNotificationChannelForApplicationAsync()
+                        .then(function(channel) {
+
+                            var user = JSON.parse(res.response);
+                            user.channel = channel.uri;
+                            WinJS.xhr({
+                                url: base_url + "usuarios/" + user.idFacebook,
+                                type: "put",  responseType: "json",
+                                headers: {"content-Type":"application/json"},
+                                data: JSON.stringify(user)
+                            }).done(function() {
+                                return NotificacionesPushDemoClient.push.
+                                    registerNative(channel.uri);
+
+
+                            });
+                        });
+                },
+                function(err) {
+                    Windows.Networking.PushNotifications.
+                        PushNotificationChannelManager.
+                        createPushNotificationChannelForApplicationAsync()
+                        .then(function(channel) {
+                            var usr = {
+                                idFacebook: facebook.id,
+                                nombre: facebook.name,
+                                channel: channel.uri
+                            };
+                        var dataTxt = JSON.stringify(usr);
+                            WinJS.xhr({
+                                url: base_url + "usuarios",
+                                type: "post",
+                                data: dataTxt,
+                                responseType: "json",
+                                headers: { "content-Type": "application/json" }
+                            }).done(function() {
+                                return NotificacionesPushDemoClient.push.
+                                    registerNative(channel.uri);
+
+
+                            },
+                                function(erro) {
+                                    var a = erro;
+                                }
+                            );
+                        });
+
+                });
+    }
+
     WinJS.UI.Pages.define("/pages/home/home.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
-            // TODO: Initialize the page here.
+            document.getElementById("btnLogin").addEventListener("click",
+                login);
+            document.getElementById("btnSubmit").addEventListener("click",
+                function () {
+                        var obj= {
+                            asunto: document.getElementById("txtAsunto").value,
+                            texto: document.getElementById("txtMensaje").value
+
+
+                        }
+
+                        NotificacionesPushDemoClient.getTable("mensaje").
+                            insert(obj).done(function() {
+                           
+
+
+                            },
+                        function () {
+                           
+
+
+                        }
+                        );
+
+                });
         }
     });
 })();
--- NotificacionesPush/services/mobileServices/scripts/NotificacionesPushDemo.push.register.js
+++ NotificacionesPush/services/mobileServices/scripts/NotificacionesPushDemo.push.register.js
@@ -8,21 +8,15 @@
     app.addEventListener("activated", function (args) {
         if (args.detail.kind == activation.ActivationKind.launch) {
             // Note: in the simulator this code will go throw the error path
-            Windows.Networking.PushNotifications.
-                PushNotificationChannelManager.
-                createPushNotificationChannelForApplicationAsync()
-                .then(function (channel) {
-                    return NotificacionesPushDemoClient.push.
-                        registerNative(channel.uri);
-                })
-                .done(function (registration) {
+          
+               /* .done(function (registration) {
                     return NotificacionesPushDemoClient
                         .invokeApi("notifyAllUsers",
                         { body: { toast: "Sample Toast" } });
                 }, function (error) {
                     // Error
 
-                });
+                })*/;
         }
     });
 })();
